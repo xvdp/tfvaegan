f282e0c5906a79ef547eb3f80f7bda87af5d9ad2
Akshita Gupta <akshita.sem.iitr@gmail.com>
2020-08-07 20:02:31 -0400

    zero-shot-action codes

--- zero-shot-images/image-scripts/run_awa_tfvaegan.py
+++ zero-shot-images/image-scripts/run_awa_tfvaegan.py
@@ -20,7 +20,7 @@ if opt.inductive:
     --gammaG 10 --gzsl --encoded_noise --manualSeed 9182 --preprocessing --cuda --image_embedding res101 \
     --class_embedding att --nepoch 400 --syn_num 1800 --ngh 4096 --ndh 4096 --lambda1 10 --critic_iter 5 --nclass_all 50 --dataroot data --dataset AWA2 \
     --batch_size 64 --nz 85 --latent_size 85 --attSize 85 --resSize 2048 --outf checkpoint_camera_ready/ --outname awa_tfvaegan_inductive \
-    --lr 0.00001 --classifier_lr 0.001 --recons_weight 0.1 --freeze_dec --cls2fast --zsl_dec --use_mult_rep \
+    --lr 0.00001 --classifier_lr 0.001 --recons_weight 0.1 --freeze_dec \
     --feed_lr 0.0001 --lambda_mult 1.1 --dec_lr 0.0001 --loop_count 2 --a1 0.01 --a2 0.01''')
     # AWA2	300	1800	0.716378563642502	0.752101491415115	0.591020912906508	0.66190201075384
 
@@ -31,7 +31,7 @@ if opt.transductive:
     --gammaG 10 --gammaG_D2 10 --gammaD2 10 --gzsl --encoded_noise --manualSeed 9182 --preprocessing --cuda --image_embedding res101 \
     --class_embedding att --nepoch 800 --syn_num 1800 --ngh 4096 --ndh 4096 --lambda1 10 --critic_iter 5 --nclass_all 50 --dataroot data --dataset AWA2 \
     --batch_size 64 --nz 85 --latent_size 85 --attSize 85 --resSize 2048 --outf checkpoint_camera_ready/ --outname awa_tfvaegan_transductive \
-    --lr 0.00001 --classifier_lr 0.001 --recons_weight 0.1 --freeze_dec --cls2fast --zsl_dec --use_mult_rep \
+    --lr 0.00001 --classifier_lr 0.001 --recons_weight 0.1 --freeze_dec \
     --feed_lr 0.0001 --lambda_mult 1.1 --dec_lr 0.0001 --loop_count 2 --a1 0.01 --a2 0.01''')
     # AWA2	800	1800	0.916665422916412	0.889723120605215	0.872039582834272	0.880792603244286
 
